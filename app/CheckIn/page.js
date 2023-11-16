166edd90cf91902bba291910f8e3a6e467d67b3d
Ashish <ashish@example.com>
2023-11-16 20:21:39 +0530

    Add existing project check in file

--- app/CheckIn/page.js
+++ app/CheckIn/page.js
@@ -1,8 +1,26 @@
+"use client"
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
 const page = () => {
+  const routes =useRouter()
+  function back(){
+    routes.back(-1)
+  }
   return (
-    <div>page</div>
+    <>
+    <section id='checkNav'>
+      <i className='bx bx-chevron-left' onClick={back}></i>
+      <h3>Check In</h3>
+    </section> 
+    <section id='checkBody'>
+    <div id='checkBox'>
+       
+    </div>
+      <p>Check in for 7 consecutive days to get treasure box and receive mysterious prizes! .</p>
+     <img src='https://fastwin.app/includes/images/Treasure_b.png'/>
+    </section>
+    </>
   )
 }
 
